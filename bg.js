27832d2a2772c7c66885d59b8ea66e2922f4bd5e
Froggy1018 <jihe2023@gmail.com>
2026-01-19 19:48:10 +0900

    Added another image to the array

--- bg.js
+++ bg.js
@@ -2,6 +2,7 @@ var background = [
     'https://i.ibb.co/RpMvh1dW/Screenshot-2025-11-06-205025.png',
     'https://i.ibb.co/1YH6tXjN/planning-japan-trip.jpg',
     'https://i.ibb.co/84c94ggg/map-of-tokyo-japan-travel-scaled.webp',
+    'https://i.ibb.co/7JtrXcrT/map-of-tokyo-japan-shinjuku-scaled.webp'
 ];
 
 var randomIndex = Math.floor(Math.random()*background.length);
